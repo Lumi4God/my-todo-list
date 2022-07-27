8225d3baeacbdd0c54a652b0a8b5cfaecb5683dd
Olumide Jones <olumideyjones@yahoo.com>
2022-07-27 16:41:02 +0200

    Updates

---
+++ precache-manifest.4b435c8c839cc4a443ef22e94c5a4811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7faa3e7038e79b48aa75",
+    "url": "/my-todo-list/static/css/main.00e6d3bd.chunk.css"
+  },
+  {
+    "revision": "7faa3e7038e79b48aa75",
+    "url": "/my-todo-list/static/js/main.7faa3e70.chunk.js"
+  },
+  {
+    "revision": "d1307dc1f2753c71ff3d",
+    "url": "/my-todo-list/static/js/1.d1307dc1.chunk.js"
+  },
+  {
+    "revision": "5f92bba598ff5dbc0bf2",
+    "url": "/my-todo-list/static/js/runtime~main.5f92bba5.js"
+  },
+  {
+    "revision": "c784298dada30fca29d1aa59ed73d1bd",
+    "url": "/my-todo-list/index.html"
+  }
+];
